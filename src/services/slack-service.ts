e682916319c6ab9a620b2654983e76519b707ea9
jihoo <jihoo.taylor@gmail.com>
2023-12-10 20:06:51 +0900

    Add file download functionality to SlackService

--- src/services/slack-service.ts
+++ src/services/slack-service.ts
@@ -5,6 +5,10 @@ import {
   Middleware,
   SlackEventMiddlewareArgs,
 } from '@slack/bolt';
+import { WebClient } from '@slack/web-api';
+import axios from 'axios';
+import path from 'path';
+import fs from 'fs';
 
 export class SlackService {
   private app: App;
@@ -27,9 +31,36 @@ export class SlackService {
   private handleFileSharedEvent: Middleware<
     SlackEventMiddlewareArgs<'file_shared'>
   > = async ({ event, client }) => {
-    const { file, channel_id } = event as FileSharedEvent;
+    try {
+      const { file, channel_id } = event as FileSharedEvent;
 
-    console.log(`A file was shared in the target channel: ${file.id}`);
+      const fileInfo = await client.files.info({
+        file: file.id,
+      });
+
+      if (!fileInfo.file || !fileInfo.file.url_private) {
+        console.log(`File info has a wrong value: ${file.id}`);
+        return;
+      }
+      const fileType = fileInfo.file.mimetype;
+
+      if (!fileType) {
+        console.log(`File type not found: ${file.id}`);
+        return;
+      }
+      if (fileType.startsWith('audio/')) {
+        console.log(`Audio file detected: ${file.id}`);
+
+        const fileDownloadedPath = await this.downloadFile(
+          client,
+          fileInfo.file.url_private
+        );
+
+        // ready to use clova speech recognition api
+      }
+    } catch (error) {
+      console.error(`Failed to handle file_shared event: ${error}`);
+    }
   };
 
   public async sendMessage(channel: string, text: string) {
@@ -72,6 +103,40 @@ export class SlackService {
       console.log('Message sent to general channel.');
     } catch (error) {
       console.error(`Failed to send message: ${error}`);
+    }
+  }
+
+  private async downloadFile(client: WebClient, url: string): Promise<string> {
+    try {
+      const resp = await axios.get(url, {
+        headers: { Authorization: `Bearer ${client.token}` },
+        responseType: 'stream',
+      });
+
+      const downloadDir = path.join(__dirname, 'download');
+      fs.mkdirSync(downloadDir, { recursive: true });
+
+      const currentDateTime = new Date().toISOString().replace(/:/g, '-');
+      const filename = path.basename(url).replace(/\.[^/.]+$/, '');
+      const extension = path.extname(url);
+      const localPath = path.join(
+        downloadDir,
+        `${filename}-${currentDateTime}${extension}`
+      );
+
+      await new Promise((resolve, reject) => {
+        const stream = fs.createWriteStream(localPath);
+        resp.data.pipe(stream);
+        console.log('Downloading file...');
+        stream.on('finish', resolve);
+        stream.on('error', reject);
+      });
+
+      console.log(`File downloaded to: ${localPath}`);
+      return localPath;
+    } catch (error) {
+      console.error(`Failed to download file: ${error}`);
+      return '';
     }
   }
 }
